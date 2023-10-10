58bcb05dd3b9d1bd97f40132dd48cbe8d2a0109a
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-10-10 20:47:44 +0600

    add simulate user loading

---
+++ plugins/usersFactory.js
@@ -0,0 +1,13 @@
+export default ({ app }, inject) => {
+  inject("usersFactory", {
+    async loadUsers(offset = 0, limit = 100) {
+      await new Promise((resolve) => setTimeout(resolve, 500)); // Задержка в 500 мс для имитации сетевого оверхеда
+
+      const users = [];
+      for (let i = 0; i < limit; i++) {
+        users.push({ id: offset + i, name: `User ${offset + i}` });
+      }
+      return users;
+    },
+  });
+};
