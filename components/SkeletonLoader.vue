8dff2b7bcd7a00c92eb358ada99ab3cf3fa625c7
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-10-11 14:04:18 +0600

    add switch theme / add skeleton

---
+++ components/SkeletonLoader.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-skeleton-loader
+    class="skeleton"
+    :elevation="4"
+    type="paragraph"
+  ></v-skeleton-loader>
+</template>
+
+<script>
+export default {
+  name: "SkeletonLoader",
+};
+</script>
+
+<style scoped>
+.skeleton {
+  margin: 20px 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <button @click="loadUsers">Загрузить пользователей</button>
+    <SkeletonLoader v-if="isLoading" />
     <ul>
       <li v-for="user in users" :key="user.id">{{ user.name }}</li>
     </ul>
@@ -8,24 +9,28 @@
 </template>
 
 <script>
+import SkeletonLoader from "../components/SkeletonLoader.vue";
+
 export default {
   name: "IndexPage",
+  components: { SkeletonLoader },
 
   data() {
     return {
       users: [],
+      isLoading: false,
     };
   },
-
   methods: {
     async loadUsers() {
+      this.isLoading = true;
       this.users = []; // Очистка текущего списка пользователей
       let offset = 0;
       const limit = 500;
       const totalUsers = 8000;
-
       while (offset < totalUsers) {
         const newUsers = await this.$usersFactory.loadUsers(offset, limit);
+        this.isLoading = false;
         this.users = this.users.concat(newUsers);
         offset += limit;
       }
